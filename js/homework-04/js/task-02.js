501c417a040199222edf3499e1543dbb36bd8ea9
vovababych <vovababych@gmail.com>
2020-07-31 19:03:04 +0300

    Added hw-04

---
+++ js/homework-04/js/task-02.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const runTaskRef = document
+  .querySelector(".run-task-02")
+  .addEventListener("click", () => {
+    const inventory = {
+      items: ["Knife", "Gas mask"],
+      add(itemName) {
+        console.log(`Adding ${itemName} to inventory`);
+
+        this.items.push(itemName);
+      },
+      remove(itemName) {
+        console.log(`Removing ${itemName} from inventory`);
+
+        this.items = this.items.filter((item) => item !== itemName);
+      },
+    };
+
+    const invokeInventoryAction = function (itemName, action) {
+      console.log(`Invoking action on ${itemName}`);
+      action(itemName);
+    };
+
+    invokeInventoryAction("Medkit", inventory.add.bind(inventory));
+    // Invoking action on Medkit
+    // Adding Medkit to inventory
+
+    console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
+
+    invokeInventoryAction("Gas mask", inventory.remove.bind(inventory));
+    // Invoking action on Gas mask
+    // Removing Gas mask from inventory
+
+    console.log(inventory.items); // ['Knife', 'Medkit']
+  });
