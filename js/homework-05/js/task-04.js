379a6db24a562fa5955b1e4cd33afe26b0d11736
vovababych <vovababych@gmail.com>
2020-08-05 14:16:31 +0300

    added task-04

--- js/homework-05/js/task-04.js
+++ js/homework-05/js/task-04.js
@@ -1,5 +1,35 @@
 "use strict";
 
 document.querySelector(".run-task-04").addEventListener("click", function () {
-  console.log(" привет мир");
+  class StringBuilder {
+    constructor(value) {
+      this._value = value;
+    }
+    get value() {
+      return this._value;
+    }
+    set value(str) {
+      this._value = str;
+    }
+    append(str) {
+      this.value = this._value + str;
+    }
+    prepend(str) {
+      this.value = str + this._value;
+    }
+    pad(str) {
+      this.append(str) + this.prepend(str);
+    }
+  }
+
+  const builder = new StringBuilder(".");
+
+  builder.append("^");
+  console.log(builder.value); // '.^'
+
+  builder.prepend("^");
+  console.log(builder.value); // '^.^'
+
+  builder.pad("=");
+  console.log(builder.value); // '=^.^='
 });
